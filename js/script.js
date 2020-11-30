cf56a2044f495ef34fa03f062024c2e746bc374c
“obaidullah-cse” <cse.obaidullah@gmailcom>
2020-11-30 18:57:49 +0600

    active setup

--- js/script.js
+++ js/script.js
@@ -1,3 +1,9 @@
-var containerEl = document.querySelector('.mixitup');
 
-            var mixer = mixitup(containerEl);
+$(document).ready(function() {
+    var containerEl = document.querySelector('.mixitup');
+    var mixer = mixitup(containerEl);
+    
+    $("button").click(function () {
+    $("button").removeClass("active");      
+});
+});
